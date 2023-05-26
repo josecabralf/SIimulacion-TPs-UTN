0e7d9f7ef450bbd729eac121a1353858985029de
Santiago Gutierrez <santi0gutierrez0@gmail.com>
2023-05-26 18:48:57 -0300

    Definida estructura pantalla

--- TP4/TP4/PantallaSimuladorPolideportivo.cs
+++ TP4/TP4/PantallaSimuladorPolideportivo.cs
@@ -17,5 +17,24 @@ namespace TP4
         }
 
 
+        private void txt_DoubleParam_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        // Permitir solo digitos, caracteres de control y un solo punto decimal o signo menos
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != ',' || ((TextBox)sender).Text.Contains(",")) &&
+                (e.KeyChar != '-' || ((TextBox)sender).Text.Length != 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_IntParam_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permitir solo digitos y caracteres de control 
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
